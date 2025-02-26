ed38bb97297120d94b33d9fd5d5cee9eadf1d392
JOSEURSO <110978290+JOSEURSO@users.noreply.github.com>
2025-02-26 08:28:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -120,3 +120,21 @@ btnFechar.forEach(button => {
         cont = 1;
     })
 })
+
+
+
+const el_text = document.querySelector("#text");
+
+(function(){
+    const getText = el_text.textContent;
+
+    const textSplit = getText.split('');
+
+    el_text.textContent = "";
+
+    textSplit.map((item,line)=> {
+        setTimeout(() => {
+            el_text.textContent += item;
+        }, 60 * line);
+    })
+})();
